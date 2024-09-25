ece576d2c81c47f28e5c2909cea0ee4240b14b0a
meldsun0 <160340597+meldsun0@users.noreply.github.com>
2024-09-25 01:36:31 +0000

    adding basic implementation of a routing table

--- core/src/main/java/samba/network/history/HistoryRoutingTable.java
+++ core/src/main/java/samba/network/history/HistoryRoutingTable.java
@@ -4,23 +4,30 @@ import org.apache.tuweni.units.bigints.UInt64;
 import samba.domain.node.NodeId;
 import samba.network.RoutingTable;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class HistoryRoutingTable implements RoutingTable {
 
 
+   private final Map<NodeId, UInt64>  radiusMap;
 
-    @Override
-    public void updateRoutingTable() {
-        
-
+    public HistoryRoutingTable(){
+        this.radiusMap = new ConcurrentHashMap<>();
     }
 
     @Override
     public void evictNode(UInt64 nodeId) {
-
+        this.radiusMap.remove(nodeId);
     }
 
     @Override
-    public void updateRadius(NodeId nodeId, int radius) {
-
+    public void updateRadius(NodeId nodeId, UInt64 radius) {
+        this.radiusMap.put(nodeId, radius);
+    }
+    
+    @Override
+    public UInt64 getRadius(NodeId nodeId){
+        return this.radiusMap.get(nodeId);
     }
 }
