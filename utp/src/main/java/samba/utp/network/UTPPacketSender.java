080d52f9e94c6f28a171b2d79437605dd98a086b
meldsun <meldsun0@gmail.com>
2025-01-30 15:42:26 -0300

    removing unused class

--- utp/src/main/java/samba/utp/network/UTPPacketSender.java
+++
@@ -1,8 +0,0 @@
-package samba.utp.network;
-
-import samba.utp.data.UtpPacket;
-
-@FunctionalInterface
-public interface UTPPacketSender {
-    void send(UtpPacket packet, TransportAddress remoteAddress);
-}
