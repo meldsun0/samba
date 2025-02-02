1f18e11a9498d3cef3f8ce4fe0a9e78e7b696904
Dsorken <113465018+Dsorken@users.noreply.github.com>
2025-02-02 04:01:01 -0700

    Update SszUInt16Test.java

--- core/src/test/java/samba/schema/primitives/SszUInt16Test.java
+++ core/src/test/java/samba/schema/primitives/SszUInt16Test.java
@@ -1,23 +1,24 @@
 package samba.schema.primitives;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
 
 import samba.domain.types.unsigned.UInt16;
+
+import org.junit.jupiter.api.Test;
 import tech.pegasys.teku.infrastructure.ssz.schema.SszPrimitiveSchema;
 
 public class SszUInt16Test {
 
-    @SuppressWarnings("unchecked")
-    @Test
-    void testSszRoundTrip() {
-        SszUInt16 sszUInt16 = SszUInt16.of(UInt16.valueOf(0x1234));
-        UInt16 value = sszUInt16.get();
-        SszPrimitiveSchema<UInt16, SszUInt16> schema = (SszPrimitiveSchema<UInt16, SszUInt16>) sszUInt16.getSchema();
-        SszUInt16 sszUInt16decoded = schema.boxed(value);
+  @SuppressWarnings("unchecked")
+  @Test
+  void testSszRoundTrip() {
+    SszUInt16 sszUInt16 = SszUInt16.of(UInt16.valueOf(0x1234));
+    UInt16 value = sszUInt16.get();
+    SszPrimitiveSchema<UInt16, SszUInt16> schema =
+        (SszPrimitiveSchema<UInt16, SszUInt16>) sszUInt16.getSchema();
+    SszUInt16 sszUInt16decoded = schema.boxed(value);
 
-        assertEquals(sszUInt16.sszSerialize(), sszUInt16decoded.sszSerialize());
-        assertEquals(sszUInt16.get(), sszUInt16decoded.get());
-
-    }
+    assertEquals(sszUInt16.sszSerialize(), sszUInt16decoded.sszSerialize());
+    assertEquals(sszUInt16.get(), sszUInt16decoded.get());
+  }
 }
