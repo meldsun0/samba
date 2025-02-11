e8e2c1bf0dc17e8a2b20a2b7779bf0139ec40857
meldsun <meldsun0@gmail.com>
2025-02-11 19:42:14 -0300

    Adding test that was failing

--- utp/src/test/java/utp/UtpChannelReadingTest.java
+++ utp/src/test/java/utp/UtpChannelReadingTest.java
@@ -14,160 +14,154 @@
  */
 package utp;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 import static samba.utp.data.bytes.UnsignedTypesUtil.longToUbyte;
 import static samba.utp.data.bytes.UnsignedTypesUtil.longToUshort;
 import static utp.data.bytes.BinaryToStringTestHelper.toBinaryString;
 
+import samba.utp.SessionState;
+import samba.utp.UTPClient;
+import samba.utp.algo.UtpAlgConfiguration;
 import samba.utp.data.UtpHeaderExtension;
 import samba.utp.data.UtpPacket;
 import samba.utp.data.UtpPacketUtils;
+import samba.utp.network.udp.UDPAddress;
+import samba.utp.network.udp.UDPTransportLayer;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.tuweni.bytes.Bytes;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ReflectionUtils;
+import org.mockito.ArgumentCaptor;
 
 public class UtpChannelReadingTest {
 
-  // TODO refacto this test
-  //    @Test
-  //    public void test() throws InterruptedException, IllegalAccessException, ExecutionException {
-  //
-  //        UtpAlgConfiguration.AUTO_ACK_SMALLER_THAN_ACK_NUMBER = false;
-  //        UtpAlgConfiguration.SKIP_PACKETS_UNTIL_ACK = 1;
-  //        UDPTransportLayer udpTransportLayer = mock(UDPTransportLayer.class);
-  //
-  //
-  //        UTPClient channel = new UTPClient(udpTransportLayer);
-  //        channel.setState(SessionState.CONNECTED);
-  //
-  //
-  //
-  //
-  //        MicroSecondsTimeStamp stamp = mock(MicroSecondsTimeStamp.class);
-  //        when(stamp.utpDifference(anyInt(), anyInt())).thenReturn(500000000);
-  //        when(stamp.timeStamp()).thenReturn(10000000000000L);
-  //        when(stamp.utpTimeStamp()).thenReturn(1251241241);
-  //
-  //        Field field2 =
-  //                ReflectionUtils.findFields(
-  //                                UTPClient.class,
-  //                                f -> f.getName().equals("timeStamper"),
-  //                                ReflectionUtils.HierarchyTraversalMode.TOP_DOWN)
-  //                        .get(0);
-  //        field2.setAccessible(true);
-  //        field2.set(channel , stamp);
-  //
-  //
-  //        Field field3 =
-  //                ReflectionUtils.findFields(
-  //                                UTPClient.class,
-  //                                f -> f.getName().equals("connection"),
-  //                                ReflectionUtils.HierarchyTraversalMode.TOP_DOWN)
-  //                        .get(0);
-  //        field3.setAccessible(true);
-  //        field3.set(channel , CompletableFuture.completedFuture(null));
-  //
-  //
-  //
-  //
-  //        // last recieved packet has seqNr. 2, next one will be packet with seqNr. 3
-  //        channel.setAckNumber(2);
-  //
-  //        /*
-  //         * argument captor on socket, which will record all invocations of socket.send(packet)
-  //         * and capture the arguments, in this case the arguments are ack packets.
-  //         */
-  //        ArgumentCaptor<UtpPacket> ackOne = ArgumentCaptor.forClass(UtpPacket.class);
-  //
-  //        try {
-  //            // order of recieving the data packets -> 3,4,6,8,5, 7
-  //            channel.receivePacket(createPacket(3)); // ack 3
-  //            channel.receivePacket(createPacket(4)); // ack 4
-  //            channel.receivePacket(createPacket(6)); // ack 4, Sack 6 -> 00000001
-  //            channel.receivePacket(createPacket(8)); // ack 4, Sack 6, 8 -> 00000101
-  //            channel.receivePacket(createPacket(5)); // ack 6, sack 8 -> 00000001
-  //            channel.receivePacket(createPacket(7)); // ack 8
-  //
-  //            ByteBuffer buffer = channel.read().get();
-  //
-  //
-  //            // verify 6 ack packets where send and capture them
-  //            verify(udpTransportLayer, times(6)).sendPacket(ackOne.capture(),
-  // ArgumentCaptor.forClass(UDPAddress.class).capture());
-  //            List<UtpPacket> allValues = ackOne.getAllValues();
-  //            Iterator<UtpPacket> iterator = allValues.iterator();
-  //
-  //            // extract utp packets from the udp packets.
-  //            UtpPacket three = iterator.next();
-  //            UtpPacket four = iterator.next();
-  //            UtpPacket six = iterator.next();
-  //            UtpPacket eight = iterator.next();
-  //            UtpPacket five = iterator.next();
-  //            UtpPacket seven = iterator.next();
-  //
-  //
-  //            // first two packets were acked normally
-  //            testPacket(three, 3, null, UtpPacketUtils.STATE);
-  //            testPacket(four, 4, null, UtpPacketUtils.STATE);
-  //
-  //            // packet six recieved before packet 5: [Ack:4; SACK:6]
-  //            String selAckSix = "00000001" + "00000000" + "00000000" + "00000000";
-  //            testPacket(six, 4, selAckSix, UtpPacketUtils.STATE);
-  //
-  //            // packet 8 recieved before packet 5: [Ack: 4; SACK:6,8]
-  //            String selAckeight = "00000101" + "00000000" + "00000000" + "00000000";
-  //            testPacket(eight, 4, selAckeight, UtpPacketUtils.STATE);
-  //
-  //            // packet 5 recieved, so everything recieved up to packet 6: [Ack:6, SACK:8]
-  //            String selAckfive = "00000001" + "00000000" + "00000000" + "00000000";
-  //            testPacket(five, 6, selAckfive, UtpPacketUtils.STATE);
-  //
-  //            // everything recieved up till packet 8, means job done: [Ack:8]
-  //            testPacket(seven, 8, null, UtpPacketUtils.STATE);
-  //
-  //
-  //            channel.stop();
-  //            buffer.flip();
-  //
-  //            // buffer should have 6'000 bytes in it
-  //            assertEquals(6000, buffer.limit());
-  //
-  //            // read from buffer
-  //            byte[] third = new byte[1000];
-  //            buffer.get(third);
-  //            byte[] fourth = new byte[1000];
-  //            buffer.get(fourth);
-  //            byte[] fifth = new byte[1000];
-  //            buffer.get(fifth);
-  //            byte[] sixt = new byte[1000];
-  //            buffer.get(sixt);
-  //            byte[] seventh = new byte[1000];
-  //            buffer.get(seventh);
-  //            byte[] eighth = new byte[1000];
-  //            buffer.get(eighth);
-  //
-  //            /*
-  //             *  test recieved data. first 1000 bytes each should == 3,
-  //             *  second 1000 bytes each should == 4
-  //             *  etc.
-  //             */
-  //            assertArrayEquals(getPayload(3), third);
-  //            assertArrayEquals(getPayload(4), fourth);
-  //            assertArrayEquals(getPayload(5), fifth);
-  //            assertArrayEquals(getPayload(6), sixt);
-  //            assertArrayEquals(getPayload(7), seventh);
-  //            assertArrayEquals(getPayload(8), eighth);
-  //
-  //            /*
-  //             * check buffer is now empty
-  //             */
-  //            assertFalse(buffer.hasRemaining());
-  //
-  //
-  //        } catch (IOException e) {
-  //            // expecting no exception.
-  //            fail("Exception occured but was not expected");
-  //            e.printStackTrace();
-  //        }
-  //    }
+  @Test
+  public void test() throws InterruptedException, IllegalAccessException, ExecutionException {
+    UtpAlgConfiguration.AUTO_ACK_SMALLER_THAN_ACK_NUMBER = false;
+    UtpAlgConfiguration.SKIP_PACKETS_UNTIL_ACK = 1;
+
+    UDPTransportLayer udpTransportLayer = mock(UDPTransportLayer.class);
+    UDPAddress remoteAddress = mock(UDPAddress.class);
+
+    UTPClient channel = new UTPClient(udpTransportLayer);
+    channel.setState(SessionState.CONNECTED);
+
+    injectPriveteField(channel, "connection", CompletableFuture.completedFuture(null));
+
+    channel.setAckNumber(
+        2); // last recieved packet has seqNr. 2, next one will be packet with seqNr. 3
+
+    /*
+     * argument captor on socket, which will record all invocations of socket.send(packet)
+     * and capture the arguments, in this case the arguments are ack packets.
+     */
+    ArgumentCaptor<UtpPacket> ackOne = ArgumentCaptor.forClass(UtpPacket.class);
+
+    try {
+      channel.receivePacket(createPacket(3), remoteAddress); // ack 3
+      channel.receivePacket(createPacket(4), remoteAddress); // ack 4
+      channel.receivePacket(createPacket(6), remoteAddress); // ack 4, Sack 6 -> 00000001
+      channel.receivePacket(createPacket(8), remoteAddress); // ack 4, Sack 6, 8 -> 00000101
+      channel.receivePacket(createPacket(5), remoteAddress); // ack 6, sack 8 -> 00000001
+      channel.receivePacket(createPacket(7), remoteAddress); // ack 8
+
+      Bytes bytesRead = channel.read().get();
+
+      // verify 6 ack packets where send and capture them
+      verify(udpTransportLayer, times(6))
+          .sendPacket(ackOne.capture(), ArgumentCaptor.forClass(UDPAddress.class).capture());
+      List<UtpPacket> allValues = ackOne.getAllValues();
+      Iterator<UtpPacket> iterator = allValues.iterator();
+
+      // extract utp packets from the udp packets.
+      UtpPacket three = iterator.next();
+      UtpPacket four = iterator.next();
+      UtpPacket six = iterator.next();
+      UtpPacket eight = iterator.next();
+      UtpPacket five = iterator.next();
+      UtpPacket seven = iterator.next();
+
+      // first two packets were acked normally
+      testPacket(three, 3, null, UtpPacketUtils.STATE);
+      testPacket(four, 4, null, UtpPacketUtils.STATE);
+
+      // packet six recieved before packet 5: [Ack:4; SACK:6]
+      String selAckSix = "00000001" + "00000000" + "00000000" + "00000000";
+      testPacket(six, 4, selAckSix, UtpPacketUtils.STATE);
+
+      // packet 8 recieved before packet 5: [Ack: 4; SACK:6,8]
+      String selAckeight = "00000101" + "00000000" + "00000000" + "00000000";
+      testPacket(eight, 4, selAckeight, UtpPacketUtils.STATE);
+
+      // packet 5 recieved, so everything recieved up to packet 6: [Ack:6, SACK:8]
+      String selAckfive = "00000001" + "00000000" + "00000000" + "00000000";
+      testPacket(five, 6, selAckfive, UtpPacketUtils.STATE);
+
+      // everything recieved up till packet 8, means job done: [Ack:8]
+      testPacket(seven, 8, null, UtpPacketUtils.STATE);
+
+      ByteBuffer buffer = ByteBuffer.wrap(bytesRead.toArray());
+
+      // buffer should have 6'000 bytes in it
+      assertEquals(6000, buffer.limit());
+
+      // read from buffer
+      byte[] third = new byte[1000];
+      buffer.get(third);
+      byte[] fourth = new byte[1000];
+      buffer.get(fourth);
+      byte[] fifth = new byte[1000];
+      buffer.get(fifth);
+      byte[] sixt = new byte[1000];
+      buffer.get(sixt);
+      byte[] seventh = new byte[1000];
+      buffer.get(seventh);
+      byte[] eighth = new byte[1000];
+      buffer.get(eighth);
+
+      /*
+       *  test recieved data. first 1000 bytes each should == 3,
+       *  second 1000 bytes each should == 4
+       *  etc.
+       */
+      assertArrayEquals(getPayload(3), third);
+      assertArrayEquals(getPayload(4), fourth);
+      assertArrayEquals(getPayload(5), fifth);
+      assertArrayEquals(getPayload(6), sixt);
+      assertArrayEquals(getPayload(7), seventh);
+      assertArrayEquals(getPayload(8), eighth);
+
+      /*
+       * check buffer is now empty
+       */
+      assertFalse(buffer.hasRemaining());
+
+    } catch (IOException e) {
+      fail("Exception occured but was not expected");
+      e.printStackTrace();
+    }
+  }
+
+  private static void injectPriveteField(Object object, String attributeName, Object attributeValue)
+      throws IllegalAccessException {
+    Field field =
+        ReflectionUtils.findFields(
+                object.getClass(),
+                f -> f.getName().equals(attributeName),
+                ReflectionUtils.HierarchyTraversalMode.TOP_DOWN)
+            .get(0);
+    field.setAccessible(true);
+    field.set(object, attributeValue);
+  }
 
   private void testPacket(UtpPacket pkt, int seq, String selAck, byte type) {
 
