e7bfa55a2f55f5070a5e4ab78f8e818d4d8da185
meldsun <meldsun0@gmail.com>
2025-02-21 12:16:51 -0300

    Adding portalhistoryLocalContent endpoint and tests

--- core/src/main/java/samba/services/jsonrpc/methods/history/PortalHistoryLocalContent.java
+++ core/src/main/java/samba/services/jsonrpc/methods/history/PortalHistoryLocalContent.java
@@ -5,34 +5,41 @@ import samba.jsonrpc.config.RpcMethod;
 import samba.jsonrpc.reponse.*;
 import samba.network.history.HistoryJsonRpcRequests;
 
+import java.util.Optional;
+
 import org.apache.tuweni.bytes.Bytes;
 
 public class PortalHistoryLocalContent implements JsonRpcMethod {
 
-  private HistoryJsonRpcRequests historyJsonRpcRequests;
+    private final HistoryJsonRpcRequests historyJsonRpcRequests;
 
-  public PortalHistoryLocalContent(final HistoryJsonRpcRequests historyJsonRpcRequests) {
-    this.historyJsonRpcRequests = historyJsonRpcRequests;
-  }
-
-  @Override
-  public String getName() {
-    return RpcMethod.PORTAL_HISTORY_LOCAL_CONTENT.getMethodName();
-  }
-
-  @Override
-  public JsonRpcResponse response(JsonRpcRequestContext requestContext) {
-    try {
-      Bytes contentKey = Bytes.fromHexString(requestContext.getRequiredParameter(0, String.class));
-      if (contentKey.isEmpty()) {
-        return new JsonRpcSuccessResponse(requestContext.getRequest().getId(), false);
-      }
-
-      String result = this.historyJsonRpcRequests.getLocalContent(ContentKey.decode(contentKey));
-      return new JsonRpcSuccessResponse(requestContext.getRequest().getId(), result);
-
-    } catch (JsonRpcParameter.JsonRpcParameterException e) {
-      return createJsonRpcInvalidRequestResponse(requestContext);
+    public PortalHistoryLocalContent(final HistoryJsonRpcRequests historyJsonRpcRequests) {
+        this.historyJsonRpcRequests = historyJsonRpcRequests;
     }
-  }
+
+    @Override
+    public String getName() {
+        return RpcMethod.PORTAL_HISTORY_LOCAL_CONTENT.getMethodName();
+    }
+
+    @Override
+    public JsonRpcResponse response(JsonRpcRequestContext requestContext) {
+        try {
+            Bytes contentKey = Bytes.fromHexString(requestContext.getRequiredParameter(0, String.class));
+
+            if (contentKey.isEmpty()) {
+                return createJsonRpcInvalidRequestResponse(requestContext);
+            }
+
+            Optional<String> result = this.historyJsonRpcRequests.getLocalContent(ContentKey.decode(contentKey));
+            if (result.isEmpty()) {
+                return new JsonRpcErrorResponse(
+                        requestContext.getRequest().getId(), RpcErrorType.CONTENT_NOT_FOUND_ERROR);
+            }
+            return new JsonRpcSuccessResponse(requestContext.getRequest().getId(), result.get());
+
+        } catch (JsonRpcParameter.JsonRpcParameterException e) {
+            return createJsonRpcInvalidRequestResponse(requestContext);
+        }
+    }
 }
