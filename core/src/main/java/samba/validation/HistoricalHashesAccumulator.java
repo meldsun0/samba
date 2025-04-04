740ce16ba96190336c30954e3a2af43f8601d598
Dsorken <113465018+Dsorken@users.noreply.github.com>
2025-04-03 20:13:18 -0600

    spotless

--- core/src/main/java/samba/validation/HistoricalHashesAccumulator.java
+++ core/src/main/java/samba/validation/HistoricalHashesAccumulator.java
@@ -1,5 +1,9 @@
 package samba.validation;
 
+import samba.schema.content.ssz.blockheader.accumulator.EpochRecordList;
+import samba.schema.content.ssz.blockheader.accumulator.HeaderRecordContainer;
+import samba.schema.content.ssz.blockheader.accumulator.HistoricalHashesAccumulatorContainer;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,32 +11,35 @@ import org.apache.tuweni.bytes.Bytes32;
 import org.apache.tuweni.units.bigints.UInt256;
 import org.hyperledger.besu.ethereum.core.BlockHeader;
 
-import samba.schema.content.ssz.blockheader.accumulator.EpochRecordList;
-import samba.schema.content.ssz.blockheader.accumulator.HeaderRecordContainer;
-import samba.schema.content.ssz.blockheader.accumulator.HistoricalHashesAccumulatorContainer;
-
 public class HistoricalHashesAccumulator {
   public static final int EPOCH_SIZE = 8192;
   public static final int MAX_HISTORICAL_EPOCHS = 2048;
 
-  public static void updateAccumulator(HistoricalHashesAccumulatorContainer accumulator, BlockHeader newBlockHeader) {
+  public static void updateAccumulator(
+      HistoricalHashesAccumulatorContainer accumulator, BlockHeader newBlockHeader) {
     UInt256 lastTotalDifficulty;
     List<HeaderRecordContainer> currentEpoch = new ArrayList<>(accumulator.getEpochRecord());
     List<Bytes32> historicalEpochs = new ArrayList<>(accumulator.getHistoricalEpochs());
     if (accumulator.getEpochRecord().isEmpty()) {
       lastTotalDifficulty = UInt256.ZERO;
     } else {
-      lastTotalDifficulty = accumulator.getEpochRecord().get(accumulator.getEpochRecord().size() - 1).getTotalDifficulty();
+      lastTotalDifficulty =
+          accumulator
+              .getEpochRecord()
+              .get(accumulator.getEpochRecord().size() - 1)
+              .getTotalDifficulty();
     }
 
     if (accumulator.getEpochRecord().size() == HistoricalHashesAccumulator.EPOCH_SIZE) {
-      EpochRecordList fullEpoch = new EpochRecordList(new ArrayList<>(accumulator.getEpochRecord()));
+      EpochRecordList fullEpoch =
+          new EpochRecordList(new ArrayList<>(accumulator.getEpochRecord()));
       Bytes32 epochHash = fullEpoch.getEncodedList().hashTreeRoot();
       historicalEpochs.add(epochHash);
       currentEpoch = new ArrayList<>();
     }
 
-    UInt256 newTotalDifficulty = lastTotalDifficulty.add(UInt256.valueOf(newBlockHeader.getDifficulty().getAsBigInteger()));
+    UInt256 newTotalDifficulty =
+        lastTotalDifficulty.add(UInt256.valueOf(newBlockHeader.getDifficulty().getAsBigInteger()));
     HeaderRecordContainer newRecord =
         new HeaderRecordContainer(newBlockHeader.getHash(), newTotalDifficulty);
     currentEpoch.add(newRecord);
